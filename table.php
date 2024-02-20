c5dc33f46de64f9d88a0cdf4bad9f9d7fc586171
masayuki-umeki <kigotoki@gmail.com>
2024-02-21 06:44:06 +0900

    add

---
+++ table.php
@@ -0,0 +1,316 @@
+<?php
+$products = [
+    [
+        "ID"=> 1,
+        "IMG_TYPE"=> "S",
+        "FILE_PATH"=> "IMG_17082610164760.png",
+        "EXTENSION"=> ".png",
+    ],
+    [
+        "ID"=> 2,
+        "IMG_TYPE"=> "S",
+        "FILE_PATH"=> "IMG_17082610164761.jpg",
+        "EXTENSION"=> ".jpg",
+    ],
+    [
+        "ID"=> 3,
+        "IMG_TYPE"=> "S",
+        "FILE_PATH"=> "IMG_17082623102310.jpg",
+        "EXTENSION"=> ".jpg",
+    ],
+    [
+        "ID"=> 4,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082630642080.jpg",
+        "EXTENSION"=> ".jpg",
+    ],
+    [
+        "ID"=> 5,
+        "IMG_TYPE"=> "S",
+        "FILE_PATH"=> "IMG_17082631110900.jpg",
+        "EXTENSION"=> ".jpg",
+    ],
+    [
+        "ID"=> 6,
+        "IMG_TYPE"=> "S",
+        "FILE_PATH"=> "IMG_17082631519160.jpg",
+        "EXTENSION"=> ".jpg",
+    ],
+    [
+        "ID"=> 7,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 8,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 9,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 10,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 11,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 12,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 13,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 14,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 15,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 16,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 17,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 18,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 19,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 20,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 21,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 22,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 23,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 24,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 25,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 26,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 27,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 28,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 29,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 30,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 31,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 32,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 33,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 34,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 35,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ],
+    [
+        "ID"=> 36,
+        "IMG_TYPE"=> "L",
+        "FILE_PATH"=> "IMG_17082666147390.jfif",
+        "EXTENSION"=> ".jfif",
+    ]
+];
+
+function getIdRows($array, $key, $index) {
+    $count = 1;
+    foreach ($array as $idx => $value) {
+        if ($idx > $index) {
+            if ($value["IMG_TYPE"] == $key) {
+                $count++;
+            } else {
+                break;
+            }
+        }
+    }
+    return $count;
+}
+
+function getExtensionRows($array, $key, $index) {
+    $count = 1;
+    foreach ($array as $idx => $value) {
+        if ($idx > $index) {
+            if ($value["EXTENSION"] == $key) {
+                $count++;
+            } else {
+                break;
+            }
+        }
+    }
+    return $count;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        th, td {
+            border: solid 1px;
+            padding: 10px;
+            text-align: center;
+            vertical-align: top;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>TYPE</th>
+                    <th>PATH</th>
+                    <th>EXTENSION</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            $prevType = "";
+            $prevExt = "";
+            foreach ($products as $index => $product) {
+                if ($prevType != $product["IMG_TYPE"]) {
+                    $idRows = getIdRows($products, $product["IMG_TYPE"], $index);
+                } else {
+                    $idRows = 0;
+                }
+                if ($prevExt != $product["EXTENSION"]) {
+                    $extRows = getExtensionRows($products, $product["EXTENSION"], $index);
+                } else {
+                    $extRows = 0;
+                }
+            ?>
+                <tr>
+                    <td><?=$product["ID"]?></td>
+                    <?php if ($idRows > 0) { ?>
+                        <td rowspan="<?=$idRows?>"><?=$product["IMG_TYPE"]?></td>
+                    <?php
+                    }
+                    ?>
+                    <td><?=$product["FILE_PATH"]?></td>
+                    <?php if ($extRows > 0) { ?>
+                        <td rowspan="<?=$extRows?>"><?=$product["EXTENSION"]?></td>
+                    <?php } ?>
+                </tr>
+            <?php
+                $prevType = $product["IMG_TYPE"];
+                $prevExt = $product["EXTENSION"];
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
